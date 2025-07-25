e7d6e52bcda0442dcc637ea34d0a34a2601cea0d
JangidVish <vishal.jangid_it22@pccoer.in>
2025-07-25 21:32:50 +0530

    Complete Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ImageUpload from "./ImageUpload";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-zinc-900 min-h-screen flex flex-col items-center p-6 text-white">
+      <h1 className="text-center text-2xl font-bold mt-6">VisionBox</h1>
+      <h3 className="text-center mt-2 text-white font-semibold">
+        {" "}
+        AI-Powered iventory Management System
+      </h3>
+      <ImageUpload />
     </div>
   );
 }
